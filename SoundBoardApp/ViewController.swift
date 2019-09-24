bba24baabe3d6845764a88ea90ca182a62aae50f
flockaveli <felix.toepfer@gmail.com>
2019-09-24 15:31:04 +1200

    Initial Commit
    This is the initial storyboard layout for the Sound Effects App.
    There is no functionality applied yet.

---
+++ SoundBoardApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SoundBoardApp
+//
+//  Created by Felix on 11/09/19.
+//  Copyright © 2019 Felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+
+}
+
